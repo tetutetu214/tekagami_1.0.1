d59f2626d349592475e815f6da6ba274984858df
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-20 18:32:37 +0900

    エラー文日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname ,presence: :true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname ,presence: :true
   
 end
