09671b5de766f1c55502b32fb33c9c2304eb76e1
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-20 19:36:40 +0900

    ユーザー単体テストコード　実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   validates :nickname ,presence: :true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 
   
 end
